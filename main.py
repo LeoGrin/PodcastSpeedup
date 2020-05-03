0ef955fdf774e182b63f4c7f8b288bdfe933230f
LeoGrin <leo.grinsztajn@polytechnique.edu>
2020-05-03 17:16:41 +0200

    replaced all pydub by sox

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from pyAudioAnalysis.audioSegmentation import speaker_diarization
 import numpy as np
-from pydub import AudioSegment
-from pydub.playback import play
-from pydub.effects import speedup
 import argparse
 import sox
 import os
-
+import time
+from tqdm import tqdm
+from speech_to_text import *
 
 
 
@@ -20,38 +19,18 @@ def make_diarization_chronological(diarization):
     conversion_table = np.argsort(indices)
     return [conversion_table[speaker] for speaker in diarization]
 
-def play_chunks(chunk_list, speaker_list):
-    """
-    Play each chunk of sound segmented by speaker (mostly for debugging purpose)
-    :param chunk_list: list of segment of sounds corresponding to each speaker
-    :param speaker_list: list of speaker (corresponds to chunk_list)
-    :return: None
-    """
-    for i, chunk in enumerate(chunk_list):
-        print("Listening to speaker {}".format(speaker_list[i]))
-        play(chunk)
+#TODO convert
 
-def sound_to_chunks(sound, diarization, chunk_size):
-    """
-
-    :param diarization: list of speaker for each time chuck
-    :param chunk_size: size of each step in diarization (in seconds)
-    :return: list of segments of sound corresponding to one speaker, list of speaker for each segment
-    """
-    diarization = np.array(diarization)
-    indice_list = np.concatenate((np.where(np.diff(diarization))[0], [len(diarization) - 1]))
-    speaker_list = np.array(diarization[indice_list]).astype("int")
-    chunk_list = []
-    for i in range(len(indice_list)):
-        if i == 0:
-            segment_left = 0
-        else:
-            segment_left = (indice_list[i - 1] + 1) * chunk_size * 1000  # left bound of the speaker segment
-            # (convert into milliseconds for pydub) (+ 1 is to count the first chunk)
-        segment_right = (indice_list[i] + 1) * chunk_size * 1000  # right bound of the speaker segment
-        chunk = sound[segment_left:segment_right]
-        chunk_list.append(chunk)
-    return chunk_list, speaker_list
+# def play_chunks(chunk_list, speaker_list):
+#     """
+#     Play each chunk of sound segmented by speaker (mostly for debugging purpose)
+#     :param chunk_list: list of segment of sounds corresponding to each speaker
+#     :param speaker_list: list of speaker (corresponds to chunk_list)
+#     :return: None
+#     """
+#     for i, chunk in enumerate(chunk_list):
+#         print("Listening to speaker {}".format(speaker_list[i]))
+#         play(chunk)
 
 def sound_to_segments(diarization, chunk_size):
     """
@@ -63,94 +42,123 @@ def sound_to_segments(diarization, chunk_size):
     indice_list = np.concatenate((np.where(np.diff(diarization))[0], [len(diarization) - 1]))
     speaker_list = np.array(diarization[indice_list]).astype("int")
     segment_list = []
-    for i in range(len(indice_list)):
+    for i in tqdm(range(len(indice_list))):
         if i == 0:
             segment_left = 0
         else:
             segment_left = (indice_list[i - 1] + 1) * chunk_size   # left bound of the speaker segment
-            # (convert into milliseconds for pydub) (+ 1 is to count the first chunk)
+            # + 1 is to count the first chunk
         segment_right = (indice_list[i] + 1) * chunk_size  # right bound of the speaker segment
         segment_list.append((segment_left, segment_right))
     return segment_list, speaker_list
 
-def transform(segment_list, speaker_list, speeds, input_file, output_file):
-    output_paths = []
-    for i, segment in enumerate(segment_list):
+def transform(segment_list, speaker_list, speeds, input_file, temp_file="temp_folder/temp"):
+    temp_paths = []
+    speed_emp = [[], []]
+    for i, segment in tqdm(enumerate(segment_list)):
         tfm = sox.Transformer()
         tfm.trim(segment[0], segment[1])
+        speed_emp[speaker_list[i]].append(float(tfm.stat(input_file)["Rough frequency"]))
         tfm.tempo(speeds[speaker_list[i]], "s")
-        tfm.build(input_file, output_file + str(i) + ".wav")
-        output_paths.append(output_file + str(i) + ".wav")
-    return output_paths
+        tfm.build(input_file, temp_file + str(i) + ".wav")
+        temp_paths.append(temp_file + str(i) + ".wav")
+    print(np.mean(speed_emp[0]))
+    print(np.mean(speed_emp[1]))
+    print(speed_emp)
+    return temp_paths
 
-def combine(output_paths):
+def combine(temp_paths, output_file="results/final_result.wav", keep_files=False):
     cmb = sox.Combiner()
-    cmb.build(output_paths, "temp_folder/final_result.wav", combine_type="concatenate")
-    for file in output_paths:
-        os.remove(file)
+    cmb.build(temp_paths, output_file, combine_type="concatenate")
+    if not keep_files:
+        for file in temp_paths:
+            os.remove(file)
+
+def speed_up(segment_list, speaker_list, speeds, input_file, temp_file="temp_folder/temp", output_file="results/final_result.wav"):
+    output_paths = transform(segment_list, speaker_list, speeds, input_file, temp_file)
+    combine(output_paths, output_file)
 
 
-
-def show_speakers(chunk_list, speaker_list, extract_length=10):
+def show_speakers_sox(input_file, segment_list, speaker_list, extract_length=10):
     """
     Play an extract of each speaker so the user to help the user choose the speeds
-    :param chunk_list: list of segment of sounds corresponding to each speaker
-    :param speaker_list: list of speaker (corresponds to chunk_list)
+    :param segment: list of segment of sounds corresponding to each speaker (tuple of time coordinate)
+    :param speaker_list: list of speaker (corresponds to segment_list)
     :param extract_length: length of extracts in seconds
     :return: None
     """
     speakers = np.unique(speaker_list)
     for speaker in speakers:
+        tfm = sox.Transformer()
         # choose the longest chunk from the speaker (this way we'll be able to take an extract for sure)
-        speaker_chunks = [chunk_list[i] for i in np.where(speaker_list == speaker)[0]]
-        speaker_chunk = speaker_chunks[np.argmax([len(chunk) for chunk in speaker_chunks])]
+        speaker_segments = [segment_list[i] for i in np.where(speaker_list == speaker)[0]]
+        speaker_segment_length = [(segment[1] - segment[0]) for segment in speaker_segments]
+        speaker_segment = speaker_segments[np.argmax(speaker_segment_length)]
+        tfm.trim(speaker_segment[0], min(speaker_segment[1], speaker_segment[0] + extract_length))
         # play an extract from this chunk
         print("Listening to speaker {}".format(speaker))
+        tfm.preview(input_file)
 
-        play(speaker_chunk[:min(extract_length*1000, len(speaker_chunk)-1)])
+def find_speaker_speeds(input_file, segment_list, speaker_list, max_length=60, min_length=40):
+    speakers_speeds = []
+    speakers = np.unique(speaker_list)
+    print(speaker_list)
+    for speaker in speakers:
+        speaker_segments = [segment_list[i] for i in np.where(speaker_list == speaker)[0]]
+        speaker_segment_length = [(segment[1] - segment[0]) for segment in speaker_segments]
+        speaker_segment_indices = np.argsort(speaker_segment_length)[::-1] # from longest to shortest
+        total_length = 0  # length of all extracts we use from this speaker
+        i = 0
+        outputs = []
+        while total_length < min_length: # we concatenate different extract of the speaker until we have enough length
+            tfm = sox.Transformer()
+            speaker_segment = speaker_segments[speaker_segment_indices[i]]
+            segment_length = min(max_length, speaker_segment[1] - speaker_segment[0])
+            total_length += segment_length
+            tfm.trim(speaker_segment[0], speaker_segment[0] + segment_length)
+            #tfm.preview(input_file)
+            tfm.build(input_file, "temp_folder/temp" + str(i) + ".wav")
+            outputs.append("temp_folder/temp" + str(i) + ".wav")
+            i += 1
+        print(outputs)
+        if len(outputs) > 1:
+            cmb = sox.Combiner()
+            cmb.build(outputs, "temp_folder/to_process.wav", combine_type="concatenate")
+            n_syllabs = speech_to_syllabs("temp_folder/to_process.wav")
+            os.remove("temp_folder/to_process.wav")
+        else:
+            n_syllabs = speech_to_syllabs(outputs[0])
+        for file in outputs:
+            os.remove(file)
+        print(n_syllabs)
+        speakers_speeds.append(n_syllabs / total_length)
 
 
-def speedup_speakers(chunk_list, speaker_list, speeds, chunk_size_speedup, crossfade):
+    print(speakers_speeds)
+
+
+
+def convert(input_file, output_format="wav"):
     """
-    :param sound: sound to be modified
-    :param chunk_list: list of segment of sounds corresponding to each speaker
-    :param speaker_list: list of speaker (corresponds to chunk_list)
-    :param speeds: list of speed for each speaker
-    :return: sound object sped up differently for each speaker
+    Convert the input file the output format if necessary and save a new file with the right format
+    :param input_file: path of input file
+    :param output_format: format to output
+    :return:  path to the  new file
     """
-    modified_chuck_list = []
-    for i, chunk in enumerate(chunk_list):
-        speaker = speaker_list[i]
-        chunk = speedup(chunk, speeds[speaker], chunk_size=chunk_size_speedup, crossfade=crossfade)
-        modified_chuck_list.append(chunk)
-    modified_sound = sum(modified_chuck_list)
-    return modified_sound
+    #TODO delete file afterward
+    if input_file[-3:] != output_format:
+        tfm = sox.Transformer()
+        new_file = input_file[:-3] + "wav"
+        tfm.build(input_file, new_file)
+    else:
+        new_file = input_file
 
-def pipeline(args):
-    #TODO : convert if not wav
-    print("loading file...")
-    sound = AudioSegment.from_wav(args.filename)
-    print("diarization...")
-    diarization = np.array(speaker_diarization(args.filename, n_speakers=args.n_speakers, mid_step=args.chunk_size, short_window=args.short_window, mid_window=args.mid_window, lda_dim=args.lda_dim)).astype("int")
-    diarization = make_diarization_chronological(diarization)
-    print("number of chunk : {}".format(len(diarization)))
-    print("Found {} speakers".format(len(np.unique(diarization))))
-    print("building segments...")
-    chunk_list, speaker_list = sound_to_chunks(sound, diarization, args.chunk_size)
-    if args.show_speakers:
-        print("showing speakers..")
-        show_speakers(chunk_list, speaker_list)
-    print("speeding up...")
-    #play_chunks(chunk_list, speaker_list)
-    result = speedup_speakers(chunk_list, speaker_list, args.speeds, args.chunk_size_speedup, args.crossfade)
-    if args.play:
-         play(result)
-    # if args.save_file:
-    #     result.export(args.save_file, format=args.format)
+    return new_file
+
 
 
 def pipeline_sox(args):
-    # TODO : convert if not wav
+    args.filename = convert(args.filename)
     print("diarization...")
     diarization = np.array(speaker_diarization(args.filename, n_speakers=args.n_speakers, mid_step=args.chunk_size,
                                                short_window=args.short_window, mid_window=args.mid_window,
@@ -159,27 +167,17 @@ def pipeline_sox(args):
     print("number of chunk : {}".format(len(diarization)))
     print("Found {} speakers".format(len(np.unique(diarization))))
     print("building segments...")
-    #chunk_list, speaker_list = sound_to_chunks(sound, diarization, args.chunk_size)
     segment_list, speaker_list = sound_to_segments(diarization, args.chunk_size)
     if args.show_speakers:
         print("showing speakers..")
-        #show_speakers(chunk_list, speaker_list)
+        show_speakers_sox(args.filename, segment_list, speaker_list)
+        print("choose speeds for each speakers")
+        args.speeds = np.array(input().split(" ")).astype(float)
+    find_speaker_speeds(args.filename, segment_list, speaker_list)
     print("speeding up...")
-    # play_chunks(chunk_list, speaker_list)
-    #result = speedup_speakers(chunk_list, speaker_list, args.speeds, args.chunk_size_speedup, args.crossfade)
-    output_paths = transform(segment_list, speaker_list, args.speeds, "audio-files/tyler.wav", "temp_folder/test")
-    combine(output_paths)
-    #if args.play:
-    #    play(result)
-    # if args.save_file:
-    #     result.export(args.save_file, format=args.format)
+    speed_up(segment_list, speaker_list, args.speeds, args.filename)
 
 
-#sound = AudioSegment.from_mp3("tyler.mp3")
-
-#filename = "audio-files/tyler.wav"
-
-#pipeline(filename)
 
 
 parser = argparse.ArgumentParser(description="Speed up podcast differently for each speaker")
@@ -189,16 +187,12 @@ parser.add_argument("-n", "--n_speakers", type=int, dest="n_speakers", help="Num
 parser.add_argument("--chunk-size", type=float, dest="chunk_size", help="Size of each chunk during speaker diarization", default=0.1)
 parser.add_argument("--show-speakers", type=bool, dest="show_speakers", help="Whether to play an extract of each speaker before asking for the speeds", default=False)
 parser.add_argument("-s", "--speeds", nargs='+', type=float, dest="speeds", help="Speed of each speaker in the extract")
-parser.add_argument("--play", type=bool, dest="play", help="Whether to play audio at the end (mostly for debugging)", default=False)
 parser.add_argument("-save", "--save-file", type=str, dest="save_file", help="Path of the file we want to save to", default=None)
-parser.add_argument("-format", type=str, dest="format", help="Format of the saved file", default="wav")
-## for testing TODO remove ?
-parser.add_argument("--chunk-size-speedup", type=float, dest="chunk_size_speedup", help="Chunk size for the speedup algorithm", default=150)
-parser.add_argument("-crossfade", type=float, dest="crossfade", help="Crossfade for the speedup algorithm", default=25)
 parser.add_argument("--short-window", type=float, dest="short_window", help="Crossfade for the speedup algorithm", default=0.05)
 parser.add_argument("--mid-window", type=float, dest="mid_window", help="Crossfade for the speedup algorithm", default=2.0)
 parser.add_argument("--lda-dim", type=int, dest="lda_dim", help="Crossfade for the speedup algorithm", default=35)
 
 args = parser.parse_args()
-
+t = time.time()
 pipeline_sox(args)
+print(time.time() - t)
